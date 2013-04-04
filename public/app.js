13e475713b5c2710c549123d7b6e07be93de7d56
Patrik Wibron <patrik@wibron.nu>
2013-04-04 18:10:08 +0200

    Change Class extend init

--- public/app.js
+++ public/app.js
@@ -5,8 +5,13 @@ require([
     _,
     Class
 ) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+    var Person = Class.extend({
+      init: function(isDancing){
+        this.dancing = isDancing;
+      },
+      dance: function(){
+        console.log('dancing');
+      }
+    });
+    console.log(new Person(true));
 });
